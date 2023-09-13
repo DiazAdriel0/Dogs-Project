169e84ff5318024fe0cc15bda8071f780de53e4b
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-13 20:48:36 -0300

    docs(api/findDogById): code documentation

--- api/src/Controllers/dogControllers/findDogById.js
+++ api/src/Controllers/dogControllers/findDogById.js
@@ -2,11 +2,12 @@ const { Dog, Temperament } = require("../../DataBases/db");
 
 const findDogById = async (idRaza) => {
   try {
+    // Find the dog that matches the received ID
     const foundDog = await Dog.findOne({
       where: { id: idRaza },
       include: Temperament,
     });
-
+    // Normalize the Temperaments property to match the API property
     const temperaments = foundDog.Temperaments?.map(
       (temperament) => temperament.dataValues?.name
     ).join(", ");
