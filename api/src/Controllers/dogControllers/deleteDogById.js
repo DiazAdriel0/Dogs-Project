743e0f242be4e5654a1cf095293e8b36a02bb6e1
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-13 16:55:55 -0300

    Doc(api/deleteDogById): code documentation

--- api/src/Controllers/dogControllers/deleteDogById.js
+++ api/src/Controllers/dogControllers/deleteDogById.js
@@ -2,6 +2,7 @@ const { Dog } = require("./../../DataBases/db");
 
 const deleteDogById = async (id) => {
   try {
+    // Delete dog by id
     return await Dog.destroy({
       where: {
         id,
