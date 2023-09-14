f3862c4ea87d477dafc1784d5ef7de3ce00caad6
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-14 20:52:41 -0300

    docs(api/updateDogTemps): code documentation

--- api/src/Controllers/dogControllers/updateDogTemps.js
+++ api/src/Controllers/dogControllers/updateDogTemps.js
@@ -2,15 +2,19 @@ const { Dog, Temperament } = require("./../../DataBases/db");
 
 const updateDogTemps = async (id, temperament) => {
   try {
+    // Find the temperament that matches with name received
     const foundTempermant = await Temperament.findOne({
       where: { name: temperament },
     });
+
     if (!foundTempermant) throw new Error("Temperament not found");
 
+    // Find the breed that matches with the id received
     const dog = await Dog.findByPk(id);
 
     if (!dog) throw new Error("Dog not found");
 
+    // Link the breed with the temperament through the intermediate table
     const updatedDog = await dog.addTemperament(foundTempermant);
 
     if (!updatedDog) throw new Error("Repeated temperaments cannot be added");
