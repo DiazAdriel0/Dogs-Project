5e6c72fc08e5edb9036688761bfe68891d3a3657
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-14 11:56:44 -0300

    docs(api/findDogByName): code documentation

--- api/src/Controllers/dogControllers/findDogByName.js
+++ api/src/Controllers/dogControllers/findDogByName.js
@@ -1,6 +1,7 @@
 const { Dog } = require("../../DataBases/db");
 const { Op } = require("sequelize");
 
+// Find the breeds that contain the name provided in the query
 const findDogByName = async (name) => {
   return await Dog.findAll({ where: { name: { [Op.iLike]: `%${name}%` } } });
 };
