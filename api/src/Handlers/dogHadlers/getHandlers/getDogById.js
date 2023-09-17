7101ef5752a375a8d9e5fb7202437b2327353f79
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-17 20:47:12 -0300

    docs(api/getDogById): code documentation

--- api/src/Handlers/dogHadlers/getHandlers/getDogById.js
+++ api/src/Handlers/dogHadlers/getHandlers/getDogById.js
@@ -2,14 +2,17 @@ const axios = require("axios");
 const { API_KEY } = process.env;
 const findDogById = require("./../../../Controllers/dogControllers/findDogById");
 
+// Dogs API URL
 const URL = `https://api.thedogapi.com/v1/breeds/`;
 
 const getDogById = async (req, res) => {
   try {
     const { idDog } = req.params;
 
+    // Find a breed that matches the id passed as a param
     const dog = isNaN(idDog) && (await findDogById(idDog));
 
+    // If the breed is found in DB, it is returned; otherwise, it is searched for in the API
     if (dog) {
       res.status(200).json(dog);
     } else {
