1817f1aa24ef51d7ccfec817eeba4a3fec461546
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-17 22:17:25 -0300

    docs(api/getDogs) code documentation

--- api/src/Handlers/dogHadlers/getHandlers/getDogs.js
+++ api/src/Handlers/dogHadlers/getHandlers/getDogs.js
@@ -3,6 +3,7 @@ const findDogByName = require("../../../Controllers/dogControllers/findDogByName
 const getAllDogs = require("./../../../Controllers/dogControllers/getAllDogs");
 const { API_KEY } = process.env;
 
+// dogs API URL
 const URL = `https://api.thedogapi.com/v1/breeds/`;
 
 const getDogs = async (req, res) => {
@@ -10,23 +11,36 @@ const getDogs = async (req, res) => {
     const { name } = req.query;
     const { data } = await axios(`${URL}?api_key=${API_KEY}`);
 
+    // if name is received for query
     if (name) {
+      // filter all API breeds to find one that matches with the received name
       const nameFilteredApi = data.filter((dog) => {
         return dog.name.toLowerCase().includes(name.toLowerCase());
       });
+
+      // find the breed that matches with the received name in the database
       const nameFilteredDB = await findDogByName(name);
+
       if (!nameFilteredApi.length && !nameFilteredDB.length)
         throw new Error(`No breed matching with ${name} search was found`);
+
+      // create an array with all breeds found
       const foundDogs = [...nameFilteredApi, ...nameFilteredDB];
+
       return res.status(200).json(foundDogs);
     } else {
+      // if the request hasn't query, response with all breeds
       const allDataBaseDogs = await getAllDogs();
+
       const allDogs = [...data, ...allDataBaseDogs];
+
+      // order allDogs before response
       allDogs.sort((a, b) => {
         if (a.name > b.name) return 1;
         if (a.name < b.name) return -1;
         return 0;
       });
+
       return res.status(200).json(allDogs);
     }
   } catch (error) {
