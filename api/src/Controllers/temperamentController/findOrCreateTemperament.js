b1f541b1a4864f4bde6b08c63d5ea36e23532418
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-19 13:41:36 -0300

    docs(api/findOrCreateTemperament): code documentation

--- api/src/Controllers/temperamentController/findOrCreateTemperament.js
+++ api/src/Controllers/temperamentController/findOrCreateTemperament.js
@@ -1,6 +1,7 @@
 const { Temperament } = require("../../DataBases/db");
 
 const findOrCreateTemperament = async (temperament) => {
+  // if the temperament was not found, create new temperament on DB
   await Temperament.findOrCreate({ where: { name: temperament } });
 };
 
