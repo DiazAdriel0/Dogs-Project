19e3430d3e3761bd3f0b58b01067e2b3fd0104ef
DiazAdriel0 <diazadriel0@gmail.com>
2023-09-28 09:56:12 -0300

    docs(handlers/temperaments): code documentation

--- api/src/Handlers/temperamentHandlers/getTemperaments.js
+++ api/src/Handlers/temperamentHandlers/getTemperaments.js
@@ -1,30 +1,31 @@
-const axios = require("axios");
-const { API_KEY } = process.env;
-const findOrCreateTemperament = require("../../Controllers/temperamentController/findOrCreateTemperament");
+const axios = require('axios')
+const { API_KEY } = process.env
+const findOrCreateTemperament = require('../../Controllers/temperamentController/findOrCreateTemperament')
 
-const URL = `https://api.thedogapi.com/v1/breeds/`;
+const URL = `https://api.thedogapi.com/v1/breeds/`
 
 const getTemperaments = async (req, res) => {
   try {
-    const { data } = await axios(`${URL}?api_key=${API_KEY}`);
-    let temperaments = [];
+    // get all dogs from API
+    const { data } = await axios(`${URL}?api_key=${API_KEY}`)
+    let temperaments = []
     data.forEach((dog) => {
       if (dog.temperament) {
         const dogTemp = dog.temperament
-          ?.split(",")
-          .map((temperament) => temperament.trim());
-        temperaments.push(...dogTemp);
+          ?.split(',')
+          .map((temperament) => temperament.trim())
+        temperaments.push(...dogTemp)
       }
-    });
-    const temperamentSet = new Set(temperaments);
-    const temperamentsArray = Array.from(temperamentSet);
+    })
+    const temperamentSet = new Set(temperaments)
+    const temperamentsArray = Array.from(temperamentSet)
     temperamentsArray.forEach(async (temperament) => {
-      await findOrCreateTemperament(temperament);
-    });
-    res.status(200).json(temperamentsArray);
+      await findOrCreateTemperament(temperament)
+    })
+    res.status(200).json(temperamentsArray)
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ error: error.message })
   }
-};
+}
 
-module.exports = getTemperaments;
+module.exports = getTemperaments
