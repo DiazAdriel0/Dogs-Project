923c52c3ea2a5f59f09546cc7f71ac34234e2dc9
DiazAdriel0 <diazadriel0@gmail.com>
2023-10-14 23:02:22 -0300

    fix(test): fix Dog.sync must be conn.sync

--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -1,201 +1,201 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { assert } = require("chai");
-const session = require("supertest-session");
-const app = require("./../../src//App/app.js");
-const { Dog, conn } = require("./../../src/DataBases/db.js");
-const request = require("supertest");
+const { assert } = require('chai')
+const session = require('supertest-session')
+const app = require('./../../src//App/app.js')
+const { Dog, conn } = require('./../../src/DataBases/db.js')
+const request = require('supertest')
 
-const agent = session(app);
+const agent = session(app)
 const dog = {
-  id: "550e8400-e29b-41d4-a716-446655440000",
-  name: "Pug",
+  id: '550e8400-e29b-41d4-a716-446655440000',
+  name: 'Pug',
   image: {
-    url: "https://ichef.bbci.co.uk/news/640/cpsprodpb/14EC6/production/_124820758_pug1.jpg",
+    url: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/14EC6/production/_124820758_pug1.jpg'
   },
-  weight: { imperial: "4 - 5", metric: "4 - 5" },
-  height: { imperial: "4 - 5", metric: "4 - 5" },
-  life_span: "8 - 10",
-  temperament: "Agile, Alert",
-  bred_for: "Anything",
-  breed_group: "Anything",
-  origin: "Anything, Anything",
-};
+  weight: { imperial: '4 - 5', metric: '4 - 5' },
+  height: { imperial: '4 - 5', metric: '4 - 5' },
+  life_span: '8 - 10',
+  temperament: 'Agile, Alert',
+  bred_for: 'Anything',
+  breed_group: 'Anything',
+  origin: 'Anything, Anything'
+}
 
-describe("Dog routes", () => {
+describe('Dog routes', () => {
   before(() =>
     conn.authenticate().catch((err) => {
-      console.error("Unable to connect to the database:", err);
+      console.error('Unable to connect to the database:', err)
     })
-  );
-  beforeEach(() => Dog.sync({ force: false }).then(() => Dog.create(dog)));
-  describe("GET /dogs", () => {
-    it("should get 200", () => agent.get("/dogs").expect(200));
-  });
-});
+  )
+  beforeEach(() => conn.sync({ force: false }).then(() => Dog.create(dog)))
+  describe('GET /dogs', () => {
+    it('should get 200', () => agent.get('/dogs').expect(200))
+  })
+})
 
-describe("GET /dogs with query name=Affenpinscher", () => {
+describe('GET /dogs with query name=Affenpinscher', () => {
   it("It should return with status 200 an object with the 'name' property", (done) => {
     request(app)
-      .get("/dogs?name=affenpinscher")
+      .get('/dogs?name=affenpinscher')
       .end((err, res) => {
-        assert.equal(res.status, 200, "The HTTP status code is not 200");
+        assert.equal(res.status, 200, 'The HTTP status code is not 200')
 
-        assert.isObject(res.body[0], "The response is not a object");
+        assert.isObject(res.body[0], 'The response is not a object')
         assert.property(
           res.body[0],
-          "name",
-          "The object does not have the expected property"
-        );
+          'name',
+          'The object does not have the expected property'
+        )
 
-        done();
-      });
-  });
+        done()
+      })
+  })
   it("It should return with status 400 an object with the 'error' property", (done) => {
     request(app)
-      .get("/dogs?name=affenpinscherInvalidQueryToSearchBreed")
+      .get('/dogs?name=affenpinscherInvalidQueryToSearchBreed')
       .end((err, res) => {
-        assert.equal(res.status, 400, "The HTTP status code is not 400");
+        assert.equal(res.status, 400, 'The HTTP status code is not 400')
 
-        assert.isObject(res.body, "The response is not a object");
+        assert.isObject(res.body, 'The response is not a object')
         assert.property(
           res.body,
-          "error",
-          "The object does not have the expected property"
-        );
+          'error',
+          'The object does not have the expected property'
+        )
 
-        done();
-      });
-  });
-});
+        done()
+      })
+  })
+})
 
-describe("GET /dogs with params :idDog", () => {
+describe('GET /dogs with params :idDog', () => {
   it("It should return with status 200 an object with the 'name' property", (done) => {
     request(app)
-      .get("/dogs/1")
+      .get('/dogs/1')
       .end((err, res) => {
-        assert.equal(res.status, 200, "The HTTP status code is not 200");
+        assert.equal(res.status, 200, 'The HTTP status code is not 200')
 
-        assert.isObject(res.body, "The response is not a object");
+        assert.isObject(res.body, 'The response is not a object')
         assert.property(
           res.body,
-          "name",
-          "The object does not have the expected property"
-        );
+          'name',
+          'The object does not have the expected property'
+        )
 
-        done();
-      });
-  });
+        done()
+      })
+  })
   it("It should return with status 400 an object with the 'error' property", (done) => {
     request(app)
-      .get("/dogs/invalidIdForSearchBreedById")
+      .get('/dogs/invalidIdForSearchBreedById')
       .end((err, res) => {
-        assert.equal(res.status, 400, "The HTTP status code is not 400");
+        assert.equal(res.status, 400, 'The HTTP status code is not 400')
 
-        assert.isObject(res.body, "The response is not a object");
+        assert.isObject(res.body, 'The response is not a object')
         assert.property(
           res.body,
-          "error",
-          "The object does not have the expected property"
-        );
+          'error',
+          'The object does not have the expected property'
+        )
 
-        done();
-      });
-  });
-});
+        done()
+      })
+  })
+})
 
-describe("POST /dogs", () => {
-  it("The response should return with status 200 an object with the properties name, image, weight, height, life_span, bred_for, breed_group, and origin", (done) => {
+describe('POST /dogs', () => {
+  it('The response should return with status 200 an object with the properties name, image, weight, height, life_span, bred_for, breed_group, and origin', (done) => {
     const newDog = {
-      name: "New Pug",
+      name: 'New Pug',
       image: {
-        url: "https://ichef.bbci.co.uk/news/640/cpsprodpb/14EC6/production/_124820758_pug1.jpg",
+        url: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/14EC6/production/_124820758_pug1.jpg'
       },
-      weight: { imperial: "4 - 5", metric: "4 - 5" },
-      height: { imperial: "4 - 5", metric: "4 - 5" },
-      life_span: "8 - 10",
-      temperament: "Agile, Alert",
-      bred_for: "Anything",
-      breed_group: "Anything",
-      origin: "Anything, Anything",
-    };
+      weight: { imperial: '4 - 5', metric: '4 - 5' },
+      height: { imperial: '4 - 5', metric: '4 - 5' },
+      life_span: '8 - 10',
+      temperament: 'Agile, Alert',
+      bred_for: 'Anything',
+      breed_group: 'Anything',
+      origin: 'Anything, Anything'
+    }
     request(app)
-      .post("/dogs")
+      .post('/dogs')
       .send(newDog)
       .end((err, res) => {
-        assert.equal(res.status, 200, "The HTTP status code is not 200");
+        assert.equal(res.status, 200, 'The HTTP status code is not 200')
 
-        assert.isObject(res.body, "The response is not a object");
+        assert.isObject(res.body, 'The response is not a object')
         assert.property(
           res.body,
-          "name",
+          'name',
           "The object does not have the property 'name'"
-        );
+        )
         assert.property(
           res.body,
-          "image",
+          'image',
           "The object does not have the property 'image'"
-        );
+        )
         assert.property(
           res.body,
-          "weight",
+          'weight',
           "The object does not have the property 'weight'"
-        );
+        )
         assert.property(
           res.body,
-          "height",
+          'height',
           "The object does not have the property 'height'"
-        );
+        )
         assert.property(
           res.body,
-          "life_span",
+          'life_span',
           "The object does not have the property 'life_span'"
-        );
+        )
         assert.property(
           res.body,
-          "bred_for",
+          'bred_for',
           "The object does not have the property 'bred_for'"
-        );
+        )
         assert.property(
           res.body,
-          "breed_group",
+          'breed_group',
           "The object does not have the property 'breed_group'"
-        );
+        )
         assert.property(
           res.body,
-          "origin",
+          'origin',
           "The object does not have the property 'origin'"
-        );
+        )
 
-        done();
-      });
+        done()
+      })
     after(async () => {
-      await Dog.destroy({ where: { name: newDog.name } });
-    });
-  });
+      await Dog.destroy({ where: { name: newDog.name } })
+    })
+  })
 
   it("It should return with status 400 an object with the 'error' property", (done) => {
     request(app)
-      .post("/dogs")
+      .post('/dogs')
       .send({})
       .end((err, res) => {
-        assert.equal(res.status, 400, "The HTTP status code is not 400");
+        assert.equal(res.status, 400, 'The HTTP status code is not 400')
 
-        assert.isObject(res.body, "The response is not a object");
+        assert.isObject(res.body, 'The response is not a object')
         assert.property(
           res.body,
-          "error",
-          "The object does not have the expected property"
-        );
+          'error',
+          'The object does not have the expected property'
+        )
 
-        done();
-      });
-  });
-});
+        done()
+      })
+  })
+})
 
-describe("GET /temperaments", () => {
-  it("should get 200", () => agent.get("/temperaments").expect(200));
-});
+describe('GET /temperaments', () => {
+  it('should get 200', () => agent.get('/temperaments').expect(200))
+})
 
 after(async () => {
-  await Dog.destroy({ where: { name: dog.name } });
-});
+  await Dog.destroy({ where: { name: dog.name } })
+})
