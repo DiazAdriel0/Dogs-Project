b44c1f0790157e1ab7909763143152d4aa29a254
DiazAdriel0 <diazadriel0@gmail.com>
2023-10-14 23:02:48 -0300

    docs(models): temperament model documentation

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -1,28 +1,29 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-  sequelize.define("Temperament", {
+  sequelize.define('Temperament', {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       unique: true,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
+        // the name property must start with an uppercase letter and can only have spaces between words
         isNameValid(value) {
-          const nameRegex = /^[A-Z][a-zA-Z]*(?:[ -][a-zA-Z]+)*$/;
+          const nameRegex = /^[A-Z][a-zA-Z]*(?:[ -][a-zA-Z]+)*$/
           if (!nameRegex.test(value)) {
             throw new Error(
               "The 'name' property does not meet the required format"
-            );
+            )
           }
-        },
-      },
-    },
-  });
-};
+        }
+      }
+    }
+  })
+}
